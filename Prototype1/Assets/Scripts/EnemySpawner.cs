06b4884fbc42521970be478c4b3b01b9b0caabeb
bunzen1012 <107540683+bunzen1012@users.noreply.github.com>
2024-11-21 18:02:30 +0900

    4-gang-Tutorial-Finished

--- Prototype1/Assets/Scripts/EnemySpawner.cs
+++ Prototype1/Assets/Scripts/EnemySpawner.cs
@@ -5,17 +5,27 @@ using UnityEngine;
 public class EnemySpawner : MonoBehaviour
 {
     public GameObject enemyPrefab;
+    public GameObject powerupPrefab;
+    public int enemyCount;
+    public int waveNumber = 1;
 
     // Start is called before the first frame update
     void Start()
     {
-        Instantiate(enemyPrefab, GenerateSpawnPosition(), enemyPrefab.transform.rotation);
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        enemyCount = FindObjectsOfType<Enemy>().Length;
+
+        if (enemyCount == 0 )
+        {
+            SpawnEenemyWave(waveNumber);
+            waveNumber++;
+            Instantiate(powerupPrefab, GenerateSpawnPosition(), powerupPrefab.transform.rotation);
+        }
     }
 
     private Vector3 GenerateSpawnPosition()
@@ -26,5 +36,13 @@ public class EnemySpawner : MonoBehaviour
         Vector3 randomPosition = new Vector3(spawnRangeX, 0,spawnRangeZ);
 
         return randomPosition;
+    }
+
+    private void SpawnEenemyWave(int enemiesToSpawn)
+    {
+        for (int i = 0; i < enemiesToSpawn; i++)
+        {
+            Instantiate(enemyPrefab, GenerateSpawnPosition(), enemyPrefab.transform.rotation);
+        }
     }
 }
